d1b09f155ff489dcf4930f9de7b6db6784ca0de0
x-inkfish-x <formerlym@outlook.com>
2018-10-25 17:36:40 +0100

    Too tired

--- VocabularyViewer.user.js
+++ VocabularyViewer.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         Vocabulary Viewer Experimental
 // @namespace    https://github.com/x-inkfish-x/
-// @version      0.1.16
+// @version      0.1.17
 // @description  A Duolinge userscript that adds a skill strength indicator
 // @author       Legato né Mikael
 // @match        https://www.duolingo.com/
 // @run-at       document-start
 
-// @downloadURL  https://github.com/x-inkfish-x/DuolingoUserscripts/raw/Experimental/VocabularyViewer.user.js
-// @updateURL    https://github.com/x-inkfish-x/DuolingoUserscripts/raw/Experimental/VocabularyViewer.user.js
+// @downloadURL  https://github.com/x-inkfish-x/DuolingoUserscripts/raw/master/VocabularyViewer.user.js
+// @updateURL    https://github.com/x-inkfish-x/DuolingoUserscripts/raw/master/VocabularyViewer.user.js
 
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
-// @require      https://raw.githubusercontent.com/x-inkfish-x/DuolingoUserscripts/Experimental/DataHelper1.0.js
+// @require      https://raw.githubusercontent.com/x-inkfish-x/DuolingoUserscripts/master/DataHelper1.0.js
 
 // ==/UserScript==
 
