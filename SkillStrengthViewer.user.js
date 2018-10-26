8be8f14802efdf65c4be8e4ad0c432eb0eb2ccf1
Unknown <formerlym@outlook.com>
2018-10-26 23:29:20 +0100

    Version increment

--- SkillStrengthViewer.user.js
+++ SkillStrengthViewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Strength Viewer Beta
 // @namespace    https://github.com/x-inkfish-x/
-// @version      2.0.5
+// @version      2.0.6
 // @description  A Duolinge userscript that adds a skill strength indicator
 // @author       Legato né Mikael
 // @match        https://www.duolingo.com/*
