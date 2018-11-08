3ba88bf73717b506a29021c291874fe8a3fcf6c6
Unknown <formerlym@outlook.com>
2018-11-08 02:11:13 +0000

    ++Version

--- NotesIndicator.user.js
+++ NotesIndicator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tips and Notes Indicator
 // @namespace    https://github.com/x-inkfish-x/
-// @version      1.0.0
+// @version      1.0.2
 // @description  A Duolingo userscripts that adds an indicator to skills with tips and notes
 // @author       Legato né Mikael
 // @match        https://www.duolingo.com/
